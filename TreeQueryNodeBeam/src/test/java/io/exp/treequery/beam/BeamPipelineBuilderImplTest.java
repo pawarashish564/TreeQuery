abf22042e87f24a6491ac4920382fc95dad2506c
dexterchan <dexterchan@gmail.com>
2020-03-09 23:34:32 +0800

    run pipeline

--- TreeQueryNodeBeam/src/test/java/io/exp/treequery/beam/BeamPipelineBuilderImplTest.java
+++ TreeQueryNodeBeam/src/test/java/io/exp/treequery/beam/BeamPipelineBuilderImplTest.java
@@ -7,19 +7,26 @@ import io.exp.treequery.cluster.NodeFactory;
 import io.exp.treequery.cluster.NodeTreeFactory;
 import io.exp.treequery.execute.*;
 import io.exp.treequery.model.Node;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.beam.sdk.Pipeline;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.List;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
-
+@Slf4j
 class BeamPipelineBuilderImplTest {
     CacheInputInterface cacheInputInterface;
     String fileName = "bondtrade1.avro";
 
-
+    String workDirectory = null;
     NodeFactory nodeFactory;
     NodeTreeFactory nodeTreeFactory;
 
@@ -28,22 +35,25 @@ class BeamPipelineBuilderImplTest {
 
         ClassLoader classLoader = ClassLoader.getSystemClassLoader();
         File jsonFile = new File(classLoader.getResource(fileName).getFile());
-        String directoryName = jsonFile.getParent();
+        workDirectory = jsonFile.getParent();
 
         cacheInputInterface = FileCacheInputImpl.builder()
-                                .fileDirectory(directoryName).build();
+                                .fileDirectory(workDirectory).build();
 
     }
 
     @Test
-    void simpleAvroReadPipeline() {
+    void simpleAvroReadPipeline() throws Exception {
         String simpleAvroTree = "SimpleAvroReadCluster.json";
         Node rootNode = null;
         nodeFactory = new TransformNodeFactory();
         nodeTreeFactory = NodeTreeFactory.builder().nodeFactory(nodeFactory).build();
         ClassLoader classLoader = ClassLoader.getSystemClassLoader();
         File jsonFile = new File(classLoader.getResource(simpleAvroTree).getFile());
-        rootNode = nodeTreeFactory.parseJsonFile(jsonFile.getAbsolutePath());
+
+        String jsonString = this.parseJsonFile(jsonFile.getAbsolutePath());
+        jsonString = jsonString.replaceAll("\\$\\{WORKDIR\\}", workDirectory);
+        rootNode = nodeTreeFactory.parseJsonString(jsonString);
 
 
         ClusterDependencyGraph.ClusterDependencyGraphBuilder clusterDependencyGraphBuilder = ClusterDependencyGraph.builder();
@@ -66,10 +76,23 @@ class BeamPipelineBuilderImplTest {
             List<Node> traversedResult = Lists.newLinkedList();
             NodeTraverser.postOrderTraversalExecution(node, null, traversedResult,nodePipeline );
             nodePipeline.getPipelineBuilder();
+            Pipeline pipeline = pipelineBuilderInterface.getPipeline();
+            pipeline.run();
             clusterDependencyGraph.removeClusterDependency(node);
         }
 
     }
 
+    private  String parseJsonFile (String jsonFile)  {
+        String jsonString="";
+        StringBuilder contentBuilder = new StringBuilder();
+        try(Stream<String> stream = Files.lines( Paths.get(jsonFile), StandardCharsets.UTF_8)){
+            stream.forEach(s -> contentBuilder.append(s));
+             jsonString= contentBuilder.toString();
+        }catch(IOException ioe){
+            log.error(ioe.getMessage());
+        }
+        return jsonString;
+    }
 
 }
