9b5945f7e923b8e9c95445673ed6cd2fc753e613
dexterchan <dexterchan@gmail.com>
2020-03-09 22:22:36 +0800

    first try avro

--- TreeQueryNodeBeam/src/main/java/io/exp/treequery/beam/transform/LoadLeafNodeHelper.java
+++ TreeQueryNodeBeam/src/main/java/io/exp/treequery/beam/transform/LoadLeafNodeHelper.java
@@ -2,6 +2,7 @@ package io.exp.treequery.beam.transform;
 
 import io.exp.treequery.Transform.LoadLeafNode;
 import io.exp.treequery.model.Node;
+import org.apache.avro.Schema;
 import org.apache.avro.generic.GenericRecord;
 import org.apache.beam.sdk.Pipeline;
 import org.apache.beam.sdk.io.AvroIO;
@@ -18,7 +19,10 @@ public class LoadLeafNodeHelper implements NodeBeamHelper{
         if (parentCollectionLst.size() > 0){
             throw new IllegalArgumentException("Parent nodes should be empty for Leaf Avro File Node");
         }
-        PCollection<GenericRecord> avroDocuments = pipeline.apply(AvroIO.read(GenericRecord.class).from(((LoadLeafNode) node).getSource()));
+        LoadLeafNode loadLeafNode = (LoadLeafNode) node;
+        Schema.Parser parser = new Schema.Parser();
+        Schema schema = parser.parse(loadLeafNode.getAvro_schema().replace("'","\""));
+        PCollection<GenericRecord> avroDocuments = pipeline.apply(AvroIO.readGenericRecords(schema).from(loadLeafNode.getSource()));
 
         return avroDocuments;
     }
--- TreeQueryNodeBeam/src/test/java/io/exp/treequery/beam/BeamPipelineBuilderImplTest.java
+++ TreeQueryNodeBeam/src/test/java/io/exp/treequery/beam/BeamPipelineBuilderImplTest.java
@@ -1,11 +1,11 @@
 package io.exp.treequery.beam;
 
+import com.google.common.collect.Lists;
 import io.exp.treequery.Transform.TransformNodeFactory;
 import io.exp.treequery.cluster.ClusterDependencyGraph;
 import io.exp.treequery.cluster.NodeFactory;
 import io.exp.treequery.cluster.NodeTreeFactory;
-import io.exp.treequery.execute.CacheInputInterface;
-import io.exp.treequery.execute.PipelineBuilderInterface;
+import io.exp.treequery.execute.*;
 import io.exp.treequery.model.Node;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -38,23 +38,36 @@ class BeamPipelineBuilderImplTest {
     @Test
     void simpleAvroReadPipeline() {
         String simpleAvroTree = "SimpleAvroReadCluster.json";
-        Node node = null;
+        Node rootNode = null;
         nodeFactory = new TransformNodeFactory();
         nodeTreeFactory = NodeTreeFactory.builder().nodeFactory(nodeFactory).build();
         ClassLoader classLoader = ClassLoader.getSystemClassLoader();
         File jsonFile = new File(classLoader.getResource(simpleAvroTree).getFile());
-        node = nodeTreeFactory.parseJsonFile(jsonFile.getAbsolutePath());
+        rootNode = nodeTreeFactory.parseJsonFile(jsonFile.getAbsolutePath());
 
-        PipelineBuilderInterface pipelineBuilderInterface = BeamPipelineBuilderImpl.builder()
-                                                            .cacheInputInterface(cacheInputInterface)
-                                                            .build();
+
         ClusterDependencyGraph.ClusterDependencyGraphBuilder clusterDependencyGraphBuilder = ClusterDependencyGraph.builder();
-        clusterDependencyGraphBuilder.constructDependencyGraph(node);
+        clusterDependencyGraphBuilder.constructDependencyGraph(rootNode);
         ClusterDependencyGraph clusterDependencyGraph = clusterDependencyGraphBuilder.build();
 
         List<Node> nodeList = null;
         nodeList = clusterDependencyGraph.findClusterWithoutDependency();
         assertThat(nodeList).hasSize(1);
+
+        for (Node node: nodeList){
+            PipelineBuilderInterface pipelineBuilderInterface = BeamPipelineBuilderImpl.builder()
+                    .cacheInputInterface(cacheInputInterface)
+                    .build();
+            NodePipeline nodePipeline = GraphNodePipeline.builder()
+                    .cluster(node.getCluster())
+                    .pipelineBuilderInterface(pipelineBuilderInterface)
+                    .cacheInputInterface(cacheInputInterface)
+                    .build();
+            List<Node> traversedResult = Lists.newLinkedList();
+            NodeTraverser.postOrderTraversalExecution(node, null, traversedResult,nodePipeline );
+            nodePipeline.getPipelineBuilder();
+            clusterDependencyGraph.removeClusterDependency(node);
+        }
 
     }
 
