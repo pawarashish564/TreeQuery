c5f5cc6d775f7715203615fb7faea77e3a33ca76
dexterchan <dexterchan@gmail.com>
2020-04-04 17:44:10 +0800

    AvroSchema drafted

--- TreeQueryNodeBeam/src/main/java/org/treequery/model/AvroSchemaHelper.java
+++ TreeQueryNodeBeam/src/main/java/org/treequery/model/AvroSchemaHelper.java
@@ -2,7 +2,7 @@ package org.treequery.model;
 
 import org.apache.avro.Schema;
 
-@FunctionalInterface
 public interface AvroSchemaHelper {
     public Schema getAvroSchema(Node node);
+    public String getAvroSchemaJsonString(Node node);
 }
