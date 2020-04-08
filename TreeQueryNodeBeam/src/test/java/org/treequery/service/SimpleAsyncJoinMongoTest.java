177ecebf3308285ce6fa700c9a48538f312f4d3f
dexterchan <dexterchan@gmail.com>
2020-04-08 19:58:42 +0800

    integration test with MOngo data join

---
+++ TreeQueryNodeBeam/src/test/java/org/treequery/service/SimpleAsyncJoinMongoTest.java
@@ -0,0 +1,86 @@
+package org.treequery.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import org.treequery.Transform.JoinNode;
+import org.treequery.beam.cache.BeamCacheOutputInterface;
+import org.treequery.model.AvroSchemaHelper;
+import org.treequery.model.BasicAvroSchemaHelper;
+import org.treequery.model.CacheTypeEnum;
+import org.treequery.model.Node;
+import org.treequery.util.AvroIOHelper;
+import org.treequery.util.GenericRecordSchemaHelper;
+import org.treequery.util.JsonInstructionHelper;
+import org.treequery.utils.TestDataAgent;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@Slf4j
+@Tag("integration")
+public class SimpleAsyncJoinMongoTest {
+    TreeQueryClusterService treeQueryClusterService = null;
+
+    BeamCacheOutputInterface beamCacheOutputInterface = null;
+    CacheTypeEnum cacheTypeEnum;
+    AvroSchemaHelper avroSchemaHelper = null;
+
+    @BeforeEach
+    public void init() throws IOException {
+        cacheTypeEnum = CacheTypeEnum.FILE;
+        avroSchemaHelper = new BasicAvroSchemaHelper();
+        beamCacheOutputInterface = new TestFileBeamCacheOutputImpl();
+    }
+
+    @Test
+    public void SimpleAsyncJoinTestWithSameCluster() throws Exception{
+        String AvroTree = "SimpleJoinMongo.json";
+        String jsonString = TestDataAgent.prepareNodeFromJsonInstruction(AvroTree);
+        Node rootNode = JsonInstructionHelper.createNode(jsonString);
+        assertThat(rootNode).isInstanceOf(JoinNode.class);
+        treeQueryClusterService =  AsyncTreeQueryClusterService.builder()
+                .treeQueryClusterRunnerFactory(()->{
+                    return TreeQueryClusterRunnerImpl.builder()
+                            .beamCacheOutputInterface(beamCacheOutputInterface)
+                            .cacheTypeEnum(cacheTypeEnum)
+                            .avroSchemaHelper(avroSchemaHelper)
+                            .build();
+                })
+                .build();
+        treeQueryClusterService.runQueryTreeNetwork(rootNode, (status)->{
+            log.debug(status.toString());
+            synchronized (rootNode) {
+                rootNode.notify();
+            }
+
+            assertThat(status.status).isEqualTo(StatusTreeQueryCluster.QueryTypeEnum.SUCCESS);
+            if(status.status!= StatusTreeQueryCluster.QueryTypeEnum.SUCCESS)
+                throw new IllegalStateException(status.toString());
+        });
+        synchronized (rootNode){
+            rootNode.wait();
+        }
+
+        //Check the avro file
+        TestFileBeamCacheOutputImpl testFileBeamCacheOutput = (TestFileBeamCacheOutputImpl) beamCacheOutputInterface;
+        File avroOutputFile = testFileBeamCacheOutput.getFile();
+        AtomicInteger counter = new AtomicInteger();
+        AvroIOHelper.readAvroGenericRecordFile(avroOutputFile,avroSchemaHelper.getAvroSchema(rootNode),
+                (record)->{
+                    assertThat(record).isNotNull();
+                    counter.incrementAndGet();
+                    String isinBondTrade = GenericRecordSchemaHelper.StringifyAvroValue(record, "bondtrade.asset.securityId");
+                    String isinSecCode = GenericRecordSchemaHelper.StringifyAvroValue(record,"bondstatic.isin_code");
+                    assertEquals(isinBondTrade, isinSecCode);
+                    assertThat(isinBondTrade.length()).isGreaterThan(5);
+                });
+
+        assertEquals(1000, counter.get());
+    }
+}
