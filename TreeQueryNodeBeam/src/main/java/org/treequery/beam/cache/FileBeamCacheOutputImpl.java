29521b9083c75dfc4201a8f661bdc74df22df069
dexterchan <dexterchan@gmail.com>
2020-04-12 13:04:48 +0800

    move generic paging logic to avroiohelper

--- TreeQueryNodeBeam/src/main/java/org/treequery/beam/cache/FileBeamCacheOutputImpl.java
+++ TreeQueryNodeBeam/src/main/java/org/treequery/beam/cache/FileBeamCacheOutputImpl.java
@@ -11,6 +11,7 @@ import org.apache.avro.generic.GenericRecordBuilder;
 import org.apache.avro.io.DatumReader;
 import org.apache.beam.sdk.io.AvroIO;
 import org.apache.beam.sdk.values.PCollection;
+import org.treequery.utils.AvroIOHelper;
 
 import java.io.File;
 import java.io.IOException;
@@ -57,39 +58,12 @@ public class FileBeamCacheOutputImpl implements BeamCacheOutputInterface {
         Schema schema;
         DatumReader<GenericRecord> datumReader = new GenericDatumReader<>();
         String readFileName = String.format("%s.avro",this.fileName);
-        if (page<1){
-            throw new IllegalArgumentException(String.format("page should be> 1 but received %d",page));
-        }
-        if (pageSize<1){
-            throw new IllegalArgumentException(String.format("pageSize should be> 1 but received %d",page));
-        }
         try {
-            DataFileReader<GenericRecord> dataFileReader = new DataFileReader<>(new File(readFileName), datumReader);
-            schema = dataFileReader.getSchema();
-            GenericRecord recordPt = null;
-            long counter = 0;
-            long lessThan = (page-1)*pageSize;
-            long GreaterThan = (page)*pageSize;
-            while (dataFileReader.hasNext()) {
-                counter ++;
-
-                recordPt = dataFileReader.next(recordPt);
-                log.debug(recordPt.toString());
-                if (counter> lessThan && counter<=GreaterThan){
-                    GenericData.Record data = (GenericData.Record) recordPt;
-                    GenericRecordBuilder genericRecordBuilder = new GenericRecordBuilder(data);
-                    dataConsumer.accept(genericRecordBuilder.build());
-                }
-                if (counter >= GreaterThan){
-                    break;
-                }
-            }
-
+            schema = AvroIOHelper.getPageRecordFromAvroFile(readFileName, pageSize, page, dataConsumer);
         }catch(IOException ioe){
             log.error(String.format("Failed to retrieve %s:%s",fileName+".avro",ioe.getMessage()));
             throw new IllegalStateException(String.format("Failed to retrieve %s:%s",fileName+".avro",ioe.getMessage()));
         }
-
         return schema;
     }
 }
--- TreeQueryStatic/src/main/java/org/treequery/cluster/Cluster.java
+++ TreeQueryStatic/src/main/java/org/treequery/cluster/Cluster.java
@@ -2,11 +2,14 @@ package org.treequery.cluster;
 
 import lombok.Builder;
 import lombok.EqualsAndHashCode;
+import lombok.ToString;
 
 import java.io.Serializable;
 
 @Builder
 @EqualsAndHashCode
+@ToString
 public class Cluster implements Serializable {
     private String clusterName;
+
 }
