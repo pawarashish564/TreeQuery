928e4e81b28c6b76055498cbea72adb5d7da5d21
rickhui <rickhui0308@gmail.com>
2020-05-17 12:26:29 +0800

    Fixed Create Table order

--- TreeQueryDiscoveryService/src/test/java/DynamoCRUDTest.java
+++ TreeQueryDiscoveryService/src/test/java/DynamoCRUDTest.java
@@ -33,8 +33,8 @@ public class DynamoCRUDTest {
                 new String[]{"-inMemory", "-port", port});
         server.start();
         dynamoDB = new DynamoClient("http://localhost:8000").getDynamoDB();
-        serviceProxy = new DiscoveryServiceProxyImpl(dynamoDB);
         createTestTable();
+        serviceProxy = new DiscoveryServiceProxyImpl(dynamoDB);
     }
 
     @AfterAll
