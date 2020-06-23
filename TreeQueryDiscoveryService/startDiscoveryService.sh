d562e5c5df05f8343e78eeaad3c0ae292982460b
dexterchan <dexterchan@gmail.com>
2020-06-23 13:59:45 +0800

    discovert service

--- TreeQueryDiscoveryService/startDiscoveryService.sh
+++ TreeQueryDiscoveryService/startDiscoveryService.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+if [ -z "$PORT" ] ; then PORT=8082; fi
+if [ -z "$REGION" ] ; then REGION="us-west-2"; fi
+
 buildVersion=1.0-SNAPSHOT
 java -jar ./TreeQueryDiscoveryService-${buildVersion}.jar -p ${PORT} -r ${REGION} \
 --accessKey=${ACCESSKEY} --secretKey=${SECRETKEY}
