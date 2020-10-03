bfc80bfb545b8ab86c47a77a0f72d87e252e798c
dexterchan <dexterchan@gmail.com>
2020-10-03 23:24:04 +0800

    add resilsency4j for exponential backoff

--- TreeQueryNodeRunner/src/main/java/org/treequery/service/RemoteProxyTreeQueryClusterRunner.java
+++ TreeQueryNodeRunner/src/main/java/org/treequery/service/RemoteProxyTreeQueryClusterRunner.java
@@ -1,19 +1,31 @@
 package org.treequery.service;
 
+import lombok.extern.slf4j.Slf4j;
 import org.treequery.model.Node;
 import org.treequery.service.proxy.TreeQueryClusterRunnerProxyInterface;
+import org.treequery.utils.AppExceptionHandler;
 
 import java.util.function.Consumer;
 
+@Slf4j
 public class RemoteProxyTreeQueryClusterRunner implements TreeQueryClusterRunner{
 
     TreeQueryClusterRunnerProxyInterface treeQueryClusterRunnerProxyInterface;
 
     @Override
-    public void runQueryTreeNetwork(Node node, Consumer<StatusTreeQueryCluster> StatusCallback) {
-        treeQueryClusterRunnerProxyInterface.runQueryTreeNetwork(
-                node, StatusCallback
-        );
+    public void runQueryTreeNetwork(Node node, Consumer<StatusTreeQueryCluster> statusCallback) {
+        try {
+            treeQueryClusterRunnerProxyInterface.runQueryTreeNetwork(
+                    node, statusCallback
+            );
+        }catch (Throwable th){
+            log.error(th.getMessage());
+            AppExceptionHandler.feedBackException2Client(
+                    statusCallback, node,
+                    th.getMessage(),
+                    StatusTreeQueryCluster.QueryTypeEnum.FAIL
+            );
+        }
     }
 
     @Override
